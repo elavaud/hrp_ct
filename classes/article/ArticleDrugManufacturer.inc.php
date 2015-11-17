2e8e3df7ad1c500b7d464c83d12775962016c72d
Edouard <edouard.lavaud@gmail.com>
2015-11-17 17:19:15 -0500

    Added class object ArticleDrugManufacturer and corresponding class DAO

---
+++ classes/article/ArticleDrugManufacturer.inc.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @defgroup article
+ */
+
+/**
+ * @file classes/article/ArticleDrugManufacturer.inc.php
+ * 
+ * 
+ * @brief ArticleDrugManufacturer class.
+ */
+
+
+class ArticleDrugManufacturer extends DataObject {
+	
+	/**
+	 * Constructor.
+	 */
+	function ArticleDrugManufacturer() {
+            parent::DataObject();
+	}
+
+	
+        /**
+	 * Get drug id.
+	 * @return int
+	 */
+	function getDrugId() {
+		return $this->getData('drugId');
+	}
+	/**
+	 * Set drug id.
+	 * @param $drugId int
+	 */
+	function setDrugId($articleId) {
+		return $this->setData('drugId', $drugId);
+	}
+        
+        
+        /**
+	 * Get name of manufacturer.
+	 * @return string
+	 */
+	function getName() {
+		return $this->getData('name');
+	}
+	/**
+	 * Set name of manufacturer.
+	 * @param $name string
+	 */
+	function setName($name) {
+		return $this->setData('name', $name);
+	}
+        
+        
+        /**
+	 * Get address of the drug manufacturer.
+	 * @return string
+	 */
+	function getAddress() {
+		return $this->getData('address');
+	}
+	/**
+	 * Set address of the drug manufacturer.
+	 * @param $address string
+	 */
+	function setAddress($address) {
+		return $this->setData('address', $address);
+	}
+}
+?>
---
+++ classes/article/ArticleDrugManufacturerDAO.inc.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * @file classes/article/ArticleDrugManufacturerDAO.inc.php
+ *
+ * @class ArticleDrugManufacturerDAO
+ *
+ * @brief Operations for retrieving and modifying ArticleDrugManufacturer objects.
+ */
+
+import('classes.article.ArticleDrugManufacturer');
+
+class ArticleDrugManufacturerDAO extends DAO {
+
+        /**
+	 * Constructor
+	 */
+	function ArticleDrugManufacturerDAO() {
+            parent::DAO();
+	}
+        
+    	/**
+	 * Get the article drug manufacturer object of a submission.
+	 * @param $articleDrugManufacturerId int
+	 * @return ArticleDrugManufacturer object
+	 */
+	function &getArticleDrugManufacturerById($articleDrugManufacturerId) {
+
+		$result =& $this->retrieve('SELECT * FROM article_drug_manu_info WHERE drug_manu_id = ?', (int) $articleDrugManufacturerId);		
+
+		$articleDrugManufacturer =& $this->_returnArticleDrugManufacturerFromRow($result->GetRowAssoc(false));
+
+		$result->Close();
+		unset($result);
+
+		return $articleDrugManufacturer;
+	}
+
+        /**
+	 * Get all article drug manufacturer objects of a drug.
+	 * @param $drugId int
+	 * @return array ArticleDrugManufacturer
+	 */
+	function &getArticleDrugManufacturersByDrugId($drugId) {
+                $articleDrugManufacturers = array();
+                
+		$result =& $this->retrieve('SELECT * FROM article_drug_manu_info WHERE drug_id = ?', (int) $drugId);		
+
+		while (!$result->EOF) {
+                    	$row =& $result->getRowAssoc(false);
+			$articleDrugManufacturers[] =& $this->_returnArticleDrugManufacturerFromRow($row);
+			$result->moveNext();
+		}
+		$result->Close();
+		unset($result);
+
+		return $articleDrugManufacturers;
+	}
+
+
+	/**
+	 * Insert a new ArticleDrugManufacturer object.
+	 * @param $articleDrugManufacturer ArticleDrugManufacturer
+	 */
+	function insertArticleDrugManufacturer(&$articleDrugManufacturer) {
+		$this->update(
+			'INSERT INTO article_drug_manu_info 
+				(drug_id, name, address)
+			VALUES
+				(?, ?, ?)',
+			array(
+				(int) $articleDrugManufacturer->getDrugId(),
+				$articleDrugManufacturer->getName(),
+				$articleDrugManufacturer->getAddress()
+			)
+		);		
+		return true;
+	}
+
+	/**
+	 * Update an existing ArticleDrugManufacturer.
+	 * @param $articleDrugManufacturer ArticleDrugManufacturer
+	 */
+	function updateArticleDrugManufacturer(&$articleDrugManufacturer) {
+		$returner = $this->update(
+			'UPDATE article_drug_manu_info
+			SET	
+				drug_id = ?,
+				name = ?,
+				address = ? 
+			WHERE	drug_manu_id = ?',
+			array(
+				(int) $articleDrugManufacturer->getDrugId(),
+                                $articleDrugManufacturer->getName(),
+                                $articleDrugManufacturer->getAddress(),
+                                $articleDrugManufacturer->getId()
+			)
+		);
+		return true;
+	}
+
+	/**
+	 * Delete a specific articleDrugManufacturer by ID
+	 * @param $articleDrugManufacturerId int
+	 */
+	function deleteArticleDrugManufacturer($articleDrugManufacturerId) {
+		
+		$returner = $this->update('DELETE FROM article_drug_manu_info WHERE drug_manu_id = ?',(int) $articleDrugManufacturerId);
+		
+		return $returner;
+	}
+
+        /**
+	 * Delete articleDrugManufacturers by drug ID
+	 * @param $drugId int
+	 */
+	function deleteArticleDrugManufacturersByArticleId($drugId) {
+		
+		$returner = $this->update('DELETE FROM article_drug_manu_info WHERE drug_id = ?',(int) $drugId);
+		
+		return $returner;
+	}
+      
+	/**
+	 * Internal function to return an articleDrugManufacturer object from a row.
+	 * @param $row array
+	 * @return articleDrugManufacturer ArticleDrugManufacturer
+	 */
+	function &_returnArticleDrugManufacturerFromRow(&$row) {
+		$articleDrugManufacturer = new ArticleDrugManufacturer();
+		$articleDrugManufacturer->setId($row['drug_manu_id']);
+		$articleDrugManufacturer->setDrugId($row['drug_id']);
+                $articleDrugManufacturer->setName($row['name']);
+                $articleDrugManufacturer->setAddress($row['address']);
+                
+		HookRegistry::call('ArticleDrugManufacturerDAO::_returnArticleDrugManufacturerFromRow', array(&$articleDrugManufacturer, &$row));
+
+		return $articleDrugManufacturer;
+	}
+}
+
+?>
